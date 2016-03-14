73e2aa6d97019d847e1e32237cc2f93823eaea69
apersinger <persingeralex@yahoo.com>
2016-03-14 13:33:50 -0400

    Committed the parent bucket file

---
+++ CRUD/classes/bucket.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: apersinger
+ * Date: 3/14/2016
+ * Time: 12:11 PM
+ */
+require_once ();
+
+class bucket
+{
+    public $mys;
+    var $bucket_id;
+
+    function __construct($id)
+    {
+        $this->bucket_id = $id;
+    }
+
+
+}
